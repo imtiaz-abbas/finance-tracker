ba949c86c1f71be39dab6615b31c3823edf4ac54
Imtiaz Abbas <imtiaz1919@gmail.com>
2018-05-19 01:20:01 +0530

    done with friends adding from ui

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'users#my_portfolio'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
+  resources :users, only: [:show]
+  resources :friendships
   resources :user_stocks, only: [:create, :destroy]
+  post 'add_friend', to: "users#add_friend"
   get 'my_friends', to: "users#my_friends"
 end
